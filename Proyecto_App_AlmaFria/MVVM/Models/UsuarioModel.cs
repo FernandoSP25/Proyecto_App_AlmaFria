e524c386591ad5d15df81aba997e5f009762f025
FernandoSP25 <fsolanoporto@gmail.com>
2024-06-05 18:44:11 -0500

    Creacion de carpeta de MVVM , Creacion de carpeta models y Viewmodels,Creacion de clases de loginn y usuarioModel,

---
+++ Proyecto_App_AlmaFria/MVVM/Models/UsuarioModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_App_AlmaFria.MVVM.Models
+{
+	public class UsuarioModel
+	{
+		public string email { get; set; }
+		public string password { get; set; }
+	}
+}
---
+++ Proyecto_App_AlmaFria/MVVM/ViewModels/LoginViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Proyecto_App_AlmaFria.MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maui.Storage;
+using System.Windows.Input;
+
+namespace Proyecto_App_AlmaFria.MVVM.ViewModels
+{
+	public class LoginViewModel : INotifyPropertyChanged
+	{
+
+		public UsuarioModel usuario { get; set; }
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public LoginViewModel()
+		{ 
+			usuario = new UsuarioModel
+			{
+				email = "MVVM",
+				password = "123"
+			};
+		}
+
+		public void btnLoginClick()
+		{
+			Application.Current.MainPage = new AppShell();
+		}
+
+
+		public void btnCrearCuenta()
+		{
+
+		}
+	}
+}
