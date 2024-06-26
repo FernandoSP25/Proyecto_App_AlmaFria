1dfa6156a1d1d55af278e6d39197164bff907525
FernandoSP25 <fsolanoporto@gmail.com>
2024-06-25 19:25:26 -0500

    Actualizaciones de  las paginas y conexciones

---
+++ Proyecto_App_AlmaFria/Utilities/DocumentOption.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_App_AlmaFria.Utilities
+{
+    public class DocumentOption
+    {
+
+		public int Key { get; set; }
+		public string Value { get; set; }
+
+		public DocumentOption(int key, string value)
+		{
+			Key = key;
+			Value = value;
+		}
+	}
+}
